94cd7021d552750382b1d42a06f0dc41eeaae4c6
flaChang <zza46585@163.com>
2021-11-11 10:54:31 +0800

    second edition

--- src/dom.js
+++ src/dom.js
@@ -4,8 +4,20 @@ window.dom = {
     },
     style(node, name, sheet) {
 
-        return node.style[name] = sheet
+        if (arguments.length === 3) {
+            node.style[name] = sheet
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                return node.style[name];
+            } else if (name instanceof Object) {
+                const object = name;
+                for (let key in object) { //for in对象时，key表示键，for in数组时，key表示下标
+                    node.style[key] = object[key];
+                }
+            }
 
+
+        }
     },
     each(node, fn) {
         for (let n = 0; n < node.length; n++) {
